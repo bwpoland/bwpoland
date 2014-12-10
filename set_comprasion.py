1c538993e2f428b92657fb6a56f2ce88ffd2144a
tomasz lach <tomaszlach86@gmail.com>
2014-12-10 18:09:03 +0100

    addet positive testcase

--- set_comprasion.py
+++ set_comprasion.py
@@ -1,7 +1,12 @@
 __author__ = 'bwpoland'
 import pytest
 
-def test_set_comparison():
+def test_set_comparison_false():
     set1 = set("1308")
     set2 = set("8035")
+    assert set1 == set2
+
+def test_set_comparison_true():
+    set1 = set("150")
+    set2 = set("150")
     assert set1 == set2
